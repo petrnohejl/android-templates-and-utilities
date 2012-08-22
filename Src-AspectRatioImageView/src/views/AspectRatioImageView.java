ad344721d7885d05dfd2b371310b99ee0397565d
Petr Nohejl <petr.nohejl@gmail.com>
2012-08-22 11:26:41 +0200

    AspectRatioImageView.

---
+++ Src-AspectRatioImageView/src/views/AspectRatioImageView.java
@@ -0,0 +1,35 @@
+package com.example.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+
+public class AspectRatioImageView extends ImageView
+{
+	public AspectRatioImageView(Context context)
+	{
+		super(context);
+	}
+
+
+	public AspectRatioImageView(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+	}
+
+
+	public AspectRatioImageView(Context context, AttributeSet attrs, int defStyle)
+	{
+		super(context, attrs, defStyle);
+	}
+
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+	{
+		int width = MeasureSpec.getSize(widthMeasureSpec);
+		int height = width * getDrawable().getIntrinsicHeight() / getDrawable().getIntrinsicWidth();
+		setMeasuredDimension(width, height);
+	}
+}
