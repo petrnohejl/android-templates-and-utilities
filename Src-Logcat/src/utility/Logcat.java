a8cfabb4d14ed9258818045d5a19f682a964136d
Petr Nohejl <petr.nohejl@gmail.com>
2012-12-11 23:27:49 +0100

    Logcat tag in static constant.

--- Src-Logcat/src/utility/Logcat.java
+++ Src-Logcat/src/utility/Logcat.java
@@ -4,11 +4,22 @@ package com.example.utility;
 public class Logcat
 {
 	public static final boolean LOG_ENABLED = true;
+	public static final String TAG = "EXAMPLE";
 	
+	
+	public static void d(String msg)
+	{
+		if(LOG_ENABLED) android.util.Log.d(TAG, msg);
+	}
 	
 	public static void d(String tag, String msg)
 	{
 		if(LOG_ENABLED) android.util.Log.d(tag, msg);
+	}
+	
+	public static void e(String msg)
+	{
+		if(LOG_ENABLED) android.util.Log.e(TAG, msg);
 	}
 	
 	public static void e(String tag, String msg)
@@ -16,9 +27,19 @@ public class Logcat
 		if(LOG_ENABLED) android.util.Log.e(tag, msg);
 	}
 	
+	public static void e(String msg, Throwable tr)
+	{
+		if(LOG_ENABLED) android.util.Log.e(TAG, msg, tr);
+	}
+	
 	public static void e(String tag, String msg, Throwable tr)
 	{
 		if(LOG_ENABLED) android.util.Log.e(tag, msg, tr);
+	}
+	
+	public static void i(String msg)
+	{
+		if(LOG_ENABLED) android.util.Log.i(TAG, msg);
 	}
 	
 	public static void i(String tag, String msg)
@@ -26,9 +47,19 @@ public class Logcat
 		if(LOG_ENABLED) android.util.Log.i(tag, msg);
 	}
 	
+	public static void v(String msg)
+	{
+		if(LOG_ENABLED) android.util.Log.v(TAG, msg);
+	}
+	
 	public static void v(String tag, String msg)
 	{
 		if(LOG_ENABLED) android.util.Log.v(tag, msg);
+	}
+	
+	public static void w(String msg)
+	{
+		if(LOG_ENABLED) android.util.Log.w(TAG, msg);
 	}
 	
 	public static void w(String tag, String msg)
