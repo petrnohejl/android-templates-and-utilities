35abc9ba8902f7c5abbe593779b70cbef2d36e17
Petr Nohejl <petr.nohejl@gmail.com>
2012-12-20 13:43:02 +0100

    Removed duplicate classes with Simple fragment.

--- Src-ListFragment/src/listener/OnLoadListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.listener;
-
-
-public interface OnLoadListener
-{
-	public void onLoad();
-}
--- Src-ListFragment/src/utility/ViewState.java
+++
@@ -1,7 +0,0 @@
-package com.example.utility;
-
-
-public class ViewState
-{
-	public static enum Visibility { CONTENT, PROGRESS, OFFLINE, EMPTY };
-}
