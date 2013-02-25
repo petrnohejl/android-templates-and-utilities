9858972ec509bef4a85f7cee3baba6992af0a1e2
Petr Nohejl <petr.nohejl@gmail.com>
2013-02-25 16:32:22 +0100

    Activity extras

---
+++ Src-Activity-Extras/src/activity/ExtrasActivity.java
@@ -0,0 +1,28 @@
+package com.example.activity;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Window;
+import com.example.R;
+
+
+public class ExtrasActivity extends SherlockFragmentActivity
+{
+	public static final String EXTRA_PRODUCT_ID = "product_id";
+	public static final String EXTRA_PRODUCT_TITLE = "product_title";
+	
+	public static final String SAVE_LIST_POSITION = "list_position";
+	public static final String SAVE_FORM_LOGIN = "form_login";
+	public static final String SAVE_FORM_PASSWORD = "form_password";
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+		setContentView(R.layout.activity_extras);
+	}
+}
