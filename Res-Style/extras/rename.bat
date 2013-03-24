b403229c05211d1986042da41a54ff921055ecef
Petr Nohejl <petr.nohejl@gmail.com>
2013-03-24 19:11:14 +0100

    Renaming script

---
+++ Res-Style/extras/rename.bat
@@ -0,0 +1,27 @@
+:: Rename files created via Android Asset Studio to my own convention
+:: Don't forget to set @color/ab_button_bg_pressed
+
+rename ab_bottom_solid_example.9.png ab_bg_split.9.png
+rename ab_solid_example.9.png ab_bg_default.9.png
+rename ab_stacked_solid_example.9.png ab_bg_stacked.9.png
+rename ab_transparent_example.9.png ab_bg_transparent.9.png
+rename btn_cab_done_default_example.9.png ab_mode_close_bg_default.9.png
+rename btn_cab_done_focused_example.9.png ab_mode_close_bg_focused.9.png
+rename btn_cab_done_pressed_example.9.png ab_mode_close_bg_pressed.9.png
+rename cab_background_bottom_example.9.png ab_bg_mode_bottom.9.png
+rename cab_background_top_example.9.png ab_bg_mode_top.9.png
+rename list_focused_example.9.png ab_button_bg_focused.9.png
+rename menu_dropdown_panel_example.9.png ab_popup_bg.9.png
+rename progress_bg_example.9.png ab_progressbar_horizontal_bg.9.png
+rename progress_primary_example.9.png ab_progressbar_horizontal_primary.9.png
+rename progress_secondary_example.9.png ab_progressbar_horizontal_secondary.9.png
+rename spinner_ab_default_example.9.png ab_dropdown_bg_default.9.png
+rename spinner_ab_disabled_example.9.png ab_dropdown_bg_disabled.9.png
+rename spinner_ab_focused_example.9.png ab_dropdown_bg_focused.9.png
+rename spinner_ab_pressed_example.9.png ab_dropdown_bg_pressed.9.png
+rename tab_selected_focused_example.9.png ab_tab_bg_selected_focused.9.png
+rename tab_selected_pressed_example.9.png ab_tab_bg_selected_pressed.9.png
+rename tab_selected_example.9.png ab_tab_bg_selected.9.png
+rename tab_unselected_focused_example.9.png ab_tab_bg_unselected_focused.9.png
+rename tab_unselected_pressed_example.9.png ab_tab_bg_unselected_pressed.9.png
+@pause
