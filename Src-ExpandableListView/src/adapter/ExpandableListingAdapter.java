14a3a0290daa0745a2d3b8c7a50d674842b2bcae
Petr Nohejl <petr.nohejl@gmail.com>
2013-03-25 19:54:37 +0100

    ExpandableListView template

---
+++ Src-ExpandableListView/src/adapter/ExpandableListingAdapter.java
@@ -0,0 +1,210 @@
+package com.example.adapter;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import com.example.R;
+import com.example.entity.Product;
+
+
+public class ExpandableListingAdapter extends BaseExpandableListAdapter
+{
+	private Context mContext;
+	private ArrayList<String> mGroups;
+	private ArrayList<ArrayList<Product>> mProducts;
+	private int mSelectedGroupPosition = -1;
+	private int mSelectedChildPosition = -1;
+	
+	
+	public ExpandableListingAdapter(Context context, ArrayList<String> groups, ArrayList<ArrayList<Product>> products)
+	{
+		mContext = context;
+		mGroups = groups;
+		mProducts = products;
+	}
+	
+	
+	@Override
+	public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent)
+	{
+		// inflate view
+		View view = convertView;
+		if(view == null) 
+		{
+			LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			view = inflater.inflate(R.layout.layout_expandable_listing_item, null);
+			
+			// view holder
+			ViewHolder holder = new ViewHolder();
+			holder.textViewName = (TextView) view.findViewById(R.id.layout_expandable_listing_item_name);
+			view.setTag(holder);
+		}
+		
+		// entity
+		Product product = (Product) getChild(groupPosition, childPosition);
+		
+		if(product != null)
+		{
+			// view holder
+			ViewHolder holder = (ViewHolder) view.getTag();
+			
+			// content
+			holder.textViewName.setText(product.getName());
+			
+			// selected item
+			if(mSelectedGroupPosition == groupPosition && mSelectedChildPosition == childPosition)
+			{
+				view.setBackgroundDrawable(mContext.getResources().getDrawable(R.color.widget_listview_item_bg_selected));
+			}
+			else
+			{
+				view.setBackgroundDrawable(mContext.getResources().getDrawable(R.drawable.selector_widget_listview_item_bg));
+			}
+		}
+		
+		return view;
+	}
+
+
+	@Override
+	public int getChildrenCount(int groupPosition) 
+	{
+		if(mProducts!=null) return mProducts.get(groupPosition).size();
+		else return 0;
+	}
+	
+	
+	@Override
+	public Object getChild(int groupPosition, int childPosition)
+	{
+		return mProducts.get(groupPosition).get(childPosition);
+	}
+
+
+	@Override
+	public long getChildId(int groupPosition, int childPosition)
+	{
+		return childPosition;
+	}
+	
+	
+	@Override
+	public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent)
+	{
+		// inflate view
+		View view = convertView;
+		if(view == null) 
+		{
+			LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			view = inflater.inflate(R.layout.layout_expandable_listing_group, null);
+
+			// view holder
+			ViewHolderGroup holder = new ViewHolderGroup();
+			holder.textViewName = (TextView) view.findViewById(R.id.layout_expandable_listing_group_name);
+			view.setTag(holder);
+		}
+
+		// entity
+		String group = (String) getGroup(groupPosition);
+
+		if(group != null)
+		{
+			// view holder
+			ViewHolderGroup holder = (ViewHolderGroup) view.getTag();
+
+			// content
+			holder.textViewName.setText(group);
+		}
+
+		return view;
+	}
+	
+	
+	@Override
+	public int getGroupCount()
+	{
+		if(mGroups!=null) return mGroups.size();
+		else return 0;
+	}
+
+
+	@Override
+	public Object getGroup(int groupPosition)
+	{
+		return mGroups.get(groupPosition);
+	}
+
+
+	@Override
+	public long getGroupId(int groupPosition)
+	{
+		return groupPosition;
+	}
+
+	
+	@Override
+	public boolean hasStableIds()
+	{
+		return true;
+	}
+
+
+	@Override
+	public boolean isChildSelectable(int groupPosition, int childPosition)
+	{
+		return true;
+	}
+	
+	
+	public void refill(Context context, ArrayList<String> groups, ArrayList<ArrayList<Product>> products)
+	{
+		mContext = context;
+		mGroups = groups;
+		mProducts = products;
+		notifyDataSetChanged();
+	}
+	
+	
+	public void stop()
+	{
+		// TODO: stop image loader
+	}
+	
+	
+	public void setSelectedPosition(int groupPosition, int childPosition)
+	{
+		mSelectedGroupPosition = groupPosition;
+		mSelectedChildPosition = childPosition;
+		notifyDataSetChanged();
+	}
+	
+	
+	public int getSelectedGroupPosition()
+	{
+		return mSelectedGroupPosition;
+	}
+	
+	
+	public int getSelectedChildPosition()
+	{
+		return mSelectedChildPosition;
+	}
+	
+	
+	static class ViewHolder
+	{
+		TextView textViewName;
+	}
+	
+	
+	static class ViewHolderGroup
+	{
+		TextView textViewName;
+	}
+}
---
+++ Src-ExpandableListView/src/fragment/ExpandableListingFragment.java
@@ -0,0 +1,392 @@
+package com.example.fragment;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.FrameLayout;
+
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+import com.example.R;
+import com.example.adapter.ExpandableListingAdapter;
+import com.example.entity.Product;
+import com.example.listener.OnLoadDataListener;
+import com.example.task.LoadDataTask;
+import com.example.task.TaskSherlockFragment;
+import com.example.utility.ViewState;
+
+
+public class ExpandableListingFragment extends TaskSherlockFragment implements OnLoadDataListener
+{
+	private boolean mActionBarProgress = false;
+	private ViewState.Visibility mViewState = null;
+	private View mRootView;
+	private ExpandableListingAdapter mAdapter;
+	private LoadDataTask mLoadDataTask;
+
+	private ArrayList<String> mGroups = new ArrayList<String>();
+	private ArrayList<ArrayList<Product>> mProducts = new ArrayList<ArrayList<Product>>();
+	
+	
+	@Override
+	public void onAttach(Activity activity) 
+	{
+		super.onAttach(activity);
+	}
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		
+		setHasOptionsMenu(true);
+		setRetainInstance(true);
+		
+		// restore saved state
+		if(savedInstanceState != null)
+		{
+			handleSavedInstanceState(savedInstanceState);
+		}
+		
+		// handle intent extras
+		Bundle extras = getActivity().getIntent().getExtras();
+		if(extras != null)
+		{
+			handleExtras(extras);
+		}
+	}
+	
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		mRootView = inflater.inflate(R.layout.layout_expandable_listing, container, false);
+		return mRootView;
+	}
+	
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState)
+	{
+		super.onActivityCreated(savedInstanceState);
+		
+		// load and show data
+		if(mViewState==null || mViewState==ViewState.Visibility.OFFLINE)
+		{
+			loadData();
+		}
+		else if(mViewState==ViewState.Visibility.CONTENT)
+		{
+			if(mGroups!=null && mProducts!=null) renderView();
+			showList();
+		}
+		else if(mViewState==ViewState.Visibility.PROGRESS)
+		{
+			showProgress();
+		}
+		
+		// progress in action bar
+		showActionBarProgress(mActionBarProgress);
+	}
+	
+	
+	@Override
+	public void onStart()
+	{
+		super.onStart();
+	}
+	
+	
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+	}
+	
+	
+	@Override
+	public void onPause()
+	{
+		super.onPause();
+		
+		// stop adapter
+		if(mAdapter!=null) mAdapter.stop();
+	}
+	
+	
+	@Override
+	public void onStop()
+	{
+		super.onStop();
+	}
+	
+	
+	@Override
+	public void onDestroyView()
+	{
+		super.onDestroyView();
+	}
+	
+	
+	@Override
+	public void onDestroy()
+	{
+		super.onDestroy();
+		
+		// cancel async tasks
+		if(mLoadDataTask!=null) mLoadDataTask.cancel(true);
+	}
+	
+	
+	@Override
+	public void onDetach()
+	{
+		super.onDetach();
+	}
+	
+	
+	@Override
+	public void onSaveInstanceState(Bundle outState)
+	{
+		// save current instance state
+		super.onSaveInstanceState(outState);
+		setUserVisibleHint(true);
+		
+		// TODO
+	}
+	
+	
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+	{
+		// action bar menu
+		super.onCreateOptionsMenu(menu, inflater);
+		
+		// TODO
+	}
+	
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) 
+	{
+		// action bar menu behaviour
+		return super.onOptionsItemSelected(item);
+		
+		// TODO
+	}
+	
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo menuInfo)
+	{
+		// context menu
+		super.onCreateContextMenu(menu, view, menuInfo);
+		
+		// TODO
+	}
+	
+	
+	@Override
+	public boolean onContextItemSelected(android.view.MenuItem item)
+	{
+		// context menu behaviour
+		return super.onContextItemSelected(item);
+		
+		// TODO
+	}
+	
+	
+	@Override
+	public void onLoadData()
+	{
+		runTaskCallback(new Runnable()
+		{
+			public void run()
+			{
+				// get data
+				for(int i=0; i<10; i++)
+				{
+					ArrayList<Product> group = new ArrayList<Product>();
+					for(int j=0; j<10; j++)
+					{
+						Product p = new Product();
+						p.setName("Product " + i + "/" + j);
+						group.add(p);
+					}
+					mProducts.add(group);
+					mGroups.add("Group " + i);
+				}
+				
+				// render view
+				if(mViewState==ViewState.Visibility.CONTENT && mAdapter!=null)
+				{
+					mAdapter.notifyDataSetChanged();
+				}
+				else
+				{
+					if(mGroups!=null && mProducts!=null) renderView();
+				}
+				
+				// hide progress
+				showList();
+			}
+		});
+	}
+	
+	
+	private void handleSavedInstanceState(Bundle savedInstanceState)
+	{
+		// TODO
+	}
+	
+	
+	private void handleExtras(Bundle extras)
+	{
+		// TODO
+	}
+	
+	
+	private void loadData()
+	{
+		if(true) // TODO: isOnline?
+		{
+			// show progress
+			showProgress();
+			
+			// run async task
+			mLoadDataTask = new LoadDataTask(this);
+			executeTask(mLoadDataTask);
+		}
+		else
+		{
+			showOffline();
+		}
+	}
+	
+	
+	private void showActionBarProgress(boolean visible)
+	{
+		// show action bar progress
+		getSherlockActivity().setSupportProgressBarIndeterminateVisibility(visible);
+		mActionBarProgress = visible;
+	}
+	
+	
+	private void showList()
+	{
+		// show list container
+		FrameLayout containerList = (FrameLayout) mRootView.findViewById(R.id.container_list);
+		FrameLayout containerProgress = (FrameLayout) mRootView.findViewById(R.id.container_progress);
+		FrameLayout containerOffline = (FrameLayout) mRootView.findViewById(R.id.container_offline);
+		containerList.setVisibility(View.VISIBLE);
+		containerProgress.setVisibility(View.GONE);
+		containerOffline.setVisibility(View.GONE);
+		mViewState = ViewState.Visibility.CONTENT;
+	}
+	
+	
+	private void showProgress()
+	{
+		// show progress container
+		FrameLayout containerList = (FrameLayout) mRootView.findViewById(R.id.container_list);
+		FrameLayout containerProgress = (FrameLayout) mRootView.findViewById(R.id.container_progress);
+		FrameLayout containerOffline = (FrameLayout) mRootView.findViewById(R.id.container_offline);
+		containerList.setVisibility(View.GONE);
+		containerProgress.setVisibility(View.VISIBLE);
+		containerOffline.setVisibility(View.GONE);
+		mViewState = ViewState.Visibility.PROGRESS;
+	}
+	
+	
+	private void showOffline()
+	{
+		// show offline container
+		FrameLayout containerList = (FrameLayout) mRootView.findViewById(R.id.container_list);
+		FrameLayout containerProgress = (FrameLayout) mRootView.findViewById(R.id.container_progress);
+		FrameLayout containerOffline = (FrameLayout) mRootView.findViewById(R.id.container_offline);
+		containerList.setVisibility(View.GONE);
+		containerProgress.setVisibility(View.GONE);
+		containerOffline.setVisibility(View.VISIBLE);
+		mViewState = ViewState.Visibility.OFFLINE;
+	}
+	
+	
+	private void renderView()
+	{
+		// reference
+		ExpandableListView listView = (ExpandableListView) mRootView.findViewById(android.R.id.list);
+		FrameLayout emptyView = (FrameLayout) mRootView.findViewById(android.R.id.empty);
+		
+		// listview content
+		if(mAdapter==null)
+		{
+			// adapter
+			mAdapter = new ExpandableListingAdapter(getActivity(), mGroups, mProducts);
+			
+			// set adapter
+			listView.setAdapter(mAdapter);
+			
+			// expand first group
+			listView.expandGroup(0);
+		}
+		else
+		{
+			// refill adapter
+			mAdapter.refill(getActivity(), mGroups, mProducts);
+			
+			// set adapter
+			listView.setAdapter(mAdapter);
+		}
+		
+		// listview item onclick
+		listView.setOnChildClickListener(new OnChildClickListener()
+		{
+			@Override
+			public boolean onChildClick(ExpandableListView parent, View view, int groupPosition, int childPosition, long id)
+			{
+				// listview item onclick
+				if(mAdapter!=null) mAdapter.setSelectedPosition(groupPosition, childPosition);
+				
+				// TODO
+				
+				return true;
+			}
+		});
+		
+		// listview empty view
+		listView.setEmptyView(emptyView);
+	}
+	
+	
+	public void expand()
+	{
+		if(mAdapter!=null && mRootView!=null)
+		{
+			ExpandableListView listView = (ExpandableListView) mRootView.findViewById(android.R.id.list);
+			int count =  mAdapter.getGroupCount();
+			for (int i = 0; i <count ; i++) listView.expandGroup(i);
+			mRootView.invalidate();
+		}
+	}
+	
+	
+	public void collapse()
+	{
+		if(mAdapter!=null && mRootView!=null)
+		{
+			ExpandableListView listView = (ExpandableListView) mRootView.findViewById(android.R.id.list);
+			int count =  mAdapter.getGroupCount();
+			for (int i = 0; i <count ; i++) listView.collapseGroup(i);
+			mRootView.invalidate();
+		}
+	}
+}
