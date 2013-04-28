d10d4f256039ccebd280f449fdf7d73c7d53f0a6
Petr Nohejl <petr.nohejl@gmail.com>
2013-04-28 21:14:40 +0200

    ObservableScrollView

---
+++ Src-ObservableScrollView/src/view/ObservableScrollView.java
@@ -0,0 +1,53 @@
+package com.example.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+
+// code taken from: http://stackoverflow.com/questions/3948934/synchronise-scrollview-scroll-positions-android
+public class ObservableScrollView extends ScrollView
+{
+	private ScrollViewListener scrollViewListener = null;
+
+
+	public ObservableScrollView(Context context)
+	{
+		super(context);
+	}
+
+
+	public ObservableScrollView(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+	}
+
+
+	public ObservableScrollView(Context context, AttributeSet attrs, int defStyle)
+	{
+		super(context, attrs, defStyle);
+	}
+
+
+	public void setOnScrollViewListener(ScrollViewListener scrollViewListener)
+	{
+		this.scrollViewListener = scrollViewListener;
+	}
+
+
+	@Override
+	protected void onScrollChanged(int x, int y, int oldx, int oldy)
+	{
+		super.onScrollChanged(x, y, oldx, oldy);
+		if(scrollViewListener != null)
+		{
+			scrollViewListener.onScrollChanged(this, x, y, oldx, oldy);
+		}
+	}
+	
+	
+	public interface ScrollViewListener
+	{
+		void onScrollChanged(ObservableScrollView scrollView, int x, int y, int oldx, int oldy);
+	}
+}
