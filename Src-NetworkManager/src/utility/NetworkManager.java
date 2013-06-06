6633c18ef14a19916ce227f869155544a03c6b84
Petr Nohejl <petr.nohejl@gmail.com>
2013-06-06 16:37:34 +0200

    NetworkManager

---
+++ Src-NetworkManager/src/utility/NetworkManager.java
@@ -0,0 +1,50 @@
+package com.example.utility;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+
+// needs android.permission.ACCESS_NETWORK_STATE
+public class NetworkManager
+{
+	public static boolean isOnline(Context context)
+	{
+		ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+		return (networkInfo!=null && networkInfo.isAvailable() && networkInfo.isConnected());
+	}
+	
+	
+	public static int getType(Context context)
+	{
+		ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+		
+		if(networkInfo!=null)
+		{
+			// returns ConnectivityManager.TYPE_WIFI, ConnectivityManager.TYPE_MOBILE etc.
+			return networkInfo.getType();
+		}
+		else
+		{
+			return -1;
+		}
+	}
+	
+	
+	public static String getTypeName(Context context)
+	{
+		ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+		
+		if(networkInfo!=null)
+		{
+			return networkInfo.getTypeName();
+		}
+		else
+		{
+			return null;
+		}
+	}
+}
