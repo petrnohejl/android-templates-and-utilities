4acc2369898dd4972dd08890fc366799661e112f
Petr Nohejl <petr.nohejl@gmail.com>
2013-06-12 17:50:48 +0200

    NewPictureReceiver

---
+++ Src-Receiver/src/receiver/NewPictureReceiver.java
@@ -0,0 +1,19 @@
+package com.example.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.example.utility.Logcat;
+
+
+public class NewPictureReceiver extends BroadcastReceiver
+{
+	@Override
+	public void onReceive(Context context, Intent intent)
+	{
+		Logcat.d("NewPictureReceiver.onReceive()");
+		
+		// TODO: do something
+	}
+}
