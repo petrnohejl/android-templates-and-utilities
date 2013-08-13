7c6ad9550f0f9c040cb14ddf801644d9e4f11dc7
Petr Nohejl <petr.nohejl@gmail.com>
2013-08-13 22:50:11 +0200

    Broadcast

---
+++ Src-Broadcast/src/broadcast/ExampleBroadcast.java
@@ -0,0 +1,20 @@
+package com.example.broadcast;
+
+import android.content.Context;
+import android.content.Intent;
+
+
+public class ExampleBroadcast
+{
+	public static final String ACTION_EXAMPLE = "example";
+	public static final String EXTRA_ARG = "arg";
+
+
+	public static void sendBroadcast(Context context, String arg)
+	{
+		Intent broadcastIntent = new Intent();
+		broadcastIntent.setAction(ACTION_EXAMPLE);
+		broadcastIntent.putExtra(EXTRA_ARG, arg);
+		context.sendBroadcast(broadcastIntent);
+	}
+}
---
+++ Src-Broadcast/src/fragment/ExampleFragment.java
@@ -0,0 +1,84 @@
+package com.example.fragment;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.SherlockFragment;
+import com.example.R;
+import com.example.broadcast.ExampleBroadcast;
+import com.example.utility.Logcat;
+
+
+public class ExampleFragment extends SherlockFragment
+{
+	private View mRootView;
+	private IntentFilter mExampleIntentFilter;
+	private BroadcastReceiver mExampleBroadcastReceiver;
+
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		mRootView = inflater.inflate(R.layout.fragment_example, container, false);
+		return mRootView;
+	}
+
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState)
+	{
+		super.onActivityCreated(savedInstanceState);
+		
+		// init receiver
+		initReceiver();
+	}
+
+
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+		
+		// register receiver
+		getActivity().registerReceiver(mExampleBroadcastReceiver, mExampleIntentFilter);
+	}
+
+
+	@Override
+	public void onPause()
+	{
+		super.onPause();
+		
+		// unregister receiver
+		getActivity().unregisterReceiver(mExampleBroadcastReceiver);
+	}
+
+
+	private void initReceiver()
+	{
+		// create intent filter
+		mExampleIntentFilter = new IntentFilter();
+		mExampleIntentFilter.addAction(ExampleBroadcast.ACTION_EXAMPLE);
+		
+		// create broadcast receiver
+		mExampleBroadcastReceiver = new BroadcastReceiver()
+		{
+			@Override
+			public void onReceive(Context context, Intent intent)
+			{
+				if(intent.getAction().equals(ExampleBroadcast.ACTION_EXAMPLE))
+				{
+					String arg = intent.getExtras().getString(ExampleBroadcast.EXTRA_ARG);
+					
+					Logcat.d("Fragment.onReceive(): " + arg);
+				}
+			}
+		};
+	}
+}
