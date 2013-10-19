d3957bd491b819f8c1f25a616291a9b22a87048e
Petr Nohejl <petr.nohejl@gmail.com>
2013-10-19 18:46:05 +0200

    Update renaming script

--- Res-Style/extras/rename.bat
+++ Res-Style/extras/rename.bat
@@ -9,11 +9,11 @@ rename btn_cab_done_focused_example.9.png ab_mode_close_bg_focused.9.png
 rename btn_cab_done_pressed_example.9.png ab_mode_close_bg_pressed.9.png
 rename cab_background_bottom_example.9.png ab_bg_mode_bottom.9.png
 rename cab_background_top_example.9.png ab_bg_mode_top.9.png
-rename list_focused_example.9.png ab_button_bg_focused.9.png
+rename list_focused_example.9.png ab_item_bg_focused.9.png
 rename menu_dropdown_panel_example.9.png ab_popup_bg.9.png
-rename progress_bg_example.9.png ab_progressbar_horizontal_bg.9.png
-rename progress_primary_example.9.png ab_progressbar_horizontal_primary.9.png
-rename progress_secondary_example.9.png ab_progressbar_horizontal_secondary.9.png
+rename progress_bg_example.9.png ab_progress_bar_horizontal_bg.9.png
+rename progress_primary_example.9.png ab_progress_bar_horizontal_primary.9.png
+rename progress_secondary_example.9.png ab_progress_bar_horizontal_secondary.9.png
 rename spinner_ab_default_example.9.png ab_dropdown_bg_default.9.png
 rename spinner_ab_disabled_example.9.png ab_dropdown_bg_disabled.9.png
 rename spinner_ab_focused_example.9.png ab_dropdown_bg_focused.9.png
