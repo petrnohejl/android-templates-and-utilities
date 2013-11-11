190f6d679944e9cff026fda1c69668784001835c
Petr Nohejl <petr.nohejl@gmail.com>
2013-11-11 18:04:29 +0100

    Navigation drawer

---
+++ Src-Navigation-Drawer/src/activity/ExampleActivity.java
@@ -0,0 +1,202 @@
+package com.example.activity;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
+import com.example.R;
+import com.example.adapter.DrawerAdapter;
+import com.example.fragment.ExampleFragment;
+
+
+public class ExampleActivity extends ActionBarActivity
+{
+	private DrawerLayout mDrawerLayout;
+	private ActionBarDrawerToggle mDrawerToggle;
+	private ListView mDrawerListView;
+	
+	private CharSequence mTitle;
+	private CharSequence mDrawerTitle;
+	private String[] mTitles;
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+		setContentView(R.layout.activity_example);
+		setActionBar();
+		setDrawer(savedInstanceState);
+	}
+	
+	
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu)
+	{
+		// action bar menu visibility
+		if(menu!=null)
+		{
+			boolean drawerOpened = mDrawerLayout.isDrawerOpen(mDrawerListView);
+			MenuItem refresh = menu.findItem(R.id.ab_button_refresh);
+			if(refresh!=null) refresh.setVisible(!drawerOpened);
+		}
+		return super.onPrepareOptionsMenu(menu);
+	}
+	
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		// open or close the drawer if home button is pressed
+		if(mDrawerToggle.onOptionsItemSelected(item))
+		{
+			return true;
+		}
+		
+		// action bar menu behaviour
+		switch (item.getItemId())
+		{
+			case android.R.id.home:
+				// TODO
+				Intent intent = new Intent(this, ExampleActivity.class);
+				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+				startActivity(intent);
+				return true;
+				
+			case R.id.ab_button_refresh:
+				// TODO
+				return true;
+			
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState)
+	{
+		super.onPostCreate(savedInstanceState);
+		mDrawerToggle.syncState();
+	}
+
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfiguration)
+	{
+		super.onConfigurationChanged(newConfiguration);
+		mDrawerToggle.onConfigurationChanged(newConfiguration);
+	}
+	
+	
+	@Override
+	public void setTitle(CharSequence title)
+	{
+		mTitle = title;
+		getSupportActionBar().setTitle(mTitle);
+	}
+	
+	
+	private void setActionBar()
+	{
+		ActionBar bar = getSupportActionBar();
+		bar.setDisplayUseLogoEnabled(false);
+		bar.setDisplayShowTitleEnabled(true);
+		bar.setDisplayShowHomeEnabled(true);
+		bar.setDisplayHomeAsUpEnabled(true);
+		bar.setHomeButtonEnabled(true);
+		
+		setSupportProgressBarIndeterminateVisibility(false);
+	}
+	
+	
+	private void setDrawer(Bundle savedInstanceState)
+	{
+		mTitle = getTitle();
+		mDrawerTitle = getTitle();
+		
+		mTitles = new String[4];
+		mTitles[0] = getString(R.string.ab_title_example);
+		mTitles[1] = getString(R.string.ab_title_example);
+		mTitles[2] = getString(R.string.ab_title_example);
+		mTitles[3] = getString(R.string.ab_title_example);
+		
+		Integer[] icons = new Integer[4];
+		icons[0] = R.drawable.ic_drawer_example;
+		icons[1] = R.drawable.ic_drawer_example;
+		icons[2] = R.drawable.ic_drawer_example;
+		icons[3] = R.drawable.ic_drawer_example;
+		
+		// reference
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.activity_example_layout);
+		mDrawerListView = (ListView) findViewById(R.id.activity_example_drawer);
+		
+		// set drawer
+		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+		mDrawerListView.setAdapter(new DrawerAdapter(this, mTitles, icons));
+		mDrawerListView.setOnItemClickListener(new OnItemClickListener()
+		{
+			@Override
+			public void onItemClick(AdapterView<?> adapterView, View clickedView, int position, long id)
+			{
+				selectDrawerItem(position);
+			}
+		});
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.drawer_toggle, R.string.drawer_open, R.string.drawer_close)
+		{
+			@Override
+			public void onDrawerClosed(View view)
+			{
+				getSupportActionBar().setTitle(mTitle);
+				supportInvalidateOptionsMenu();
+			}
+			
+			@Override
+			public void onDrawerOpened(View drawerView)
+			{
+				getSupportActionBar().setTitle(mDrawerTitle);
+				supportInvalidateOptionsMenu();
+			}
+		};
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+		
+		// show initial fragment
+		if(savedInstanceState == null)
+		{
+			selectDrawerItem(0);
+		}
+	}
+	
+	
+	private void selectDrawerItem(int position)
+	{
+		Fragment fragment;
+		if(position==0) fragment = new ExampleFragment();
+		else if(position==2) fragment = new ExampleFragment();
+		else if(position==3) fragment = new ExampleFragment();
+		else fragment = new ExampleFragment();
+		
+		FragmentManager fragmentManager = getSupportFragmentManager();
+		fragmentManager.beginTransaction().replace(R.id.activity_example_container, fragment).commitAllowingStateLoss();
+		
+		mDrawerListView.setItemChecked(position, true);
+		setTitle(mTitles[position]);
+		mDrawerLayout.closeDrawer(mDrawerListView);
+	}
+}
---
+++ Src-Navigation-Drawer/src/adapter/DrawerAdapter.java
@@ -0,0 +1,100 @@
+package com.example.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.R;
+
+
+public class DrawerAdapter extends BaseAdapter
+{
+	private Context mContext;
+	private String[] mTitleList;
+	private Integer[] mIconList;
+	
+	
+	public DrawerAdapter(Context context, String[] titleList, Integer[] iconList)
+	{
+		mContext = context;
+		mTitleList = titleList;
+		mIconList = iconList;
+	}
+	
+	
+	@Override
+	public View getView(final int position, View convertView, ViewGroup parent)
+	{
+		// inflate view
+		View view = convertView;
+		if(view == null) 
+		{
+			LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			view = inflater.inflate(R.layout.drawer_item, parent, false);
+			
+			// view holder
+			ViewHolder holder = new ViewHolder();
+			holder.titleTextView = (TextView) view.findViewById(R.id.drawer_item_title);
+			holder.iconImageView = (ImageView) view.findViewById(R.id.drawer_item_icon);
+			view.setTag(holder);
+		}
+		
+		// entity
+		String title = (String) mTitleList[position];
+		Integer icon = (Integer) mIconList[position];
+		
+		if(title!=null && icon!=null )
+		{
+			// view holder
+			ViewHolder holder = (ViewHolder) view.getTag();
+			
+			// content
+			holder.titleTextView.setText(title);
+			holder.iconImageView.setImageResource(icon);
+		}
+		
+		return view;
+	}
+	
+	
+	@Override
+	public int getCount()
+	{
+		return mTitleList.length;
+	}
+	
+	
+	@Override
+	public Object getItem(int position)
+	{
+		if(mTitleList!=null) return mTitleList[position];
+		else return null;
+	}
+	
+	
+	@Override
+	public long getItemId(int position)
+	{
+		return 0;
+	}
+	
+	
+	public void refill(Context context, String[] titleList, Integer[] iconList)
+	{
+		mContext = context;
+		mTitleList = titleList;
+		mIconList = iconList;
+		notifyDataSetChanged();
+	}
+	
+	
+	static class ViewHolder
+	{
+		TextView titleTextView;
+		ImageView iconImageView;
+	}
+}
