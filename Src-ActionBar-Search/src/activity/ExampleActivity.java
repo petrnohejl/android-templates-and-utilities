82beeb4f1462f20f1ec2a3e86c717153ebfd6f45
Petr Nohejl <petr.nohejl@gmail.com>
2014-01-02 12:32:09 +0100

    Search suggestion

--- Src-ActionBar-Search/src/activity/ExampleActivity.java
+++ Src-ActionBar-Search/src/activity/ExampleActivity.java
@@ -1,21 +1,35 @@
 package com.example.activity;
 
+import android.app.SearchManager;
+import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.os.Bundle;
+import android.provider.BaseColumns;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.SearchView;
+import android.support.v7.widget.SearchView.OnQueryTextListener;
+import android.support.v7.widget.SearchView.OnSuggestionListener;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.R;
+import com.example.adapter.SearchSuggestionAdapter;
 
 
 public class ExampleActivity extends ActionBarActivity
 {
+	private final String[] SEARCH_SUGGESTION_CURSOR_COLUMNS = { BaseColumns._ID, SearchManager.SUGGEST_COLUMN_TEXT_1, SearchManager.SUGGEST_COLUMN_TEXT_2 };
+	
+	private SearchView mSearchView;
+	private SearchSuggestionAdapter mSearchSuggestionAdapter;
+	
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
@@ -33,13 +47,13 @@ public class ExampleActivity extends ActionBarActivity
 		menuInflater.inflate(R.menu.menu_example, menu);
 		
 		// search view
-		SearchView searchView = new SearchView(getSupportActionBar().getThemedContext());
-		setupSearchView(searchView);
+		mSearchView = new SearchView(getSupportActionBar().getThemedContext());
+		setupSearchView(mSearchView);
 		
 		// search menu item
 		MenuItem searchMenuItem = menu.add(Menu.NONE, Menu.NONE, 1, R.string.ab_button_search);
 		searchMenuItem.setIcon(R.drawable.ic_menu_search);
-		MenuItemCompat.setActionView(searchMenuItem, searchView);
+		MenuItemCompat.setActionView(searchMenuItem, mSearchView);
 		MenuItemCompat.setShowAsAction(searchMenuItem, MenuItem.SHOW_AS_ACTION_ALWAYS | MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
 		
 		return super.onCreateOptionsMenu(menu);
@@ -63,5 +77,78 @@ public class ExampleActivity extends ActionBarActivity
 		AutoCompleteTextView searchText = (AutoCompleteTextView) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
 		searchText.setTextColor(getResources().getColor(R.color.global_text_primary));
 		searchText.setHintTextColor(getResources().getColor(R.color.global_text_secondary));
+		
+		// suggestion listeners
+		searchView.setOnQueryTextListener(new OnQueryTextListener()
+		{
+			@Override
+			public boolean onQueryTextSubmit(String query)
+			{
+				// TODO
+				Toast.makeText(ExampleActivity.this, "search: " + query, Toast.LENGTH_SHORT).show();
+				
+				return true;
+			}
+			
+			@Override
+			public boolean onQueryTextChange(String query)
+			{
+				if(query.length()>2)
+				{
+					updateSearchSuggestion(query);
+				}
+				return true;
+			}
+		});
+		searchView.setOnSuggestionListener(new OnSuggestionListener()
+		{
+			@Override
+			public boolean onSuggestionSelect(int position)
+			{
+				return false;
+			}
+			
+			@Override
+			public boolean onSuggestionClick(int position)
+			{
+				Cursor cursor = (Cursor) mSearchSuggestionAdapter.getItem(position);
+				String title = cursor.getString(cursor.getColumnIndex(SearchManager.SUGGEST_COLUMN_TEXT_1));
+				String subtitle = cursor.getString(cursor.getColumnIndex(SearchManager.SUGGEST_COLUMN_TEXT_2));
+				
+				// TODO
+				Toast.makeText(ExampleActivity.this, "suggestion: " + title + " / " + subtitle, Toast.LENGTH_SHORT).show();
+				
+				return true;
+			}
+		});
+	}
+	
+	
+	private void updateSearchSuggestion(String query)
+	{
+		// cursor
+		// TODO
+		MatrixCursor cursor = new MatrixCursor(SEARCH_SUGGESTION_CURSOR_COLUMNS);
+		cursor.addRow(new String[] { "1", query + "ica", "Lorem" });
+		cursor.addRow(new String[] { "2", query + "dam", "Ipsum" });
+		cursor.addRow(new String[] { "3", query + "rap", "Dolor" });
+		
+		// searchview content
+		if(mSearchSuggestionAdapter==null)
+		{
+			// create adapter
+			mSearchSuggestionAdapter = new SearchSuggestionAdapter(this, cursor);
+			
+			// set adapter
+			mSearchView.setSuggestionsAdapter(mSearchSuggestionAdapter);
+		}
+		else
+		{
+			// refill adapter
+			mSearchSuggestionAdapter.refill(this, cursor);
+			
+			// set adapter
+			mSearchView.setSuggestionsAdapter(mSearchSuggestionAdapter);
+		}
 	}
 }
---
+++ Src-ActionBar-Search/src/adapter/SearchSuggestionAdapter.java
@@ -0,0 +1,53 @@
+package com.example.adapter;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.R;
+
+
+public class SearchSuggestionAdapter extends CursorAdapter
+{
+	public SearchSuggestionAdapter(Context context, Cursor cursor)
+	{
+		super(context, cursor, 0);
+	}
+
+
+	@Override
+	public View newView(Context context, Cursor cursor, ViewGroup parent)
+	{
+		LayoutInflater inflater = LayoutInflater.from(context);
+		View view = inflater.inflate(R.layout.search_suggestion_item, parent, false);
+		return view;
+	}
+
+
+	@Override
+	public void bindView(View view, Context context, Cursor cursor)
+	{
+		// reference
+		LinearLayout root = (LinearLayout) view;
+		TextView titleTextView = (TextView) root.findViewById(R.id.search_suggestion_item_title);
+		TextView subtitleTextView = (TextView) root.findViewById(R.id.search_suggestion_item_subtitle);
+		
+		// content
+		final int index1 = cursor.getColumnIndex(SearchManager.SUGGEST_COLUMN_TEXT_1);
+		final int index2 = cursor.getColumnIndex(SearchManager.SUGGEST_COLUMN_TEXT_2);
+		titleTextView.setText(cursor.getString(index1));
+		subtitleTextView.setText(cursor.getString(index2));
+	}
+	
+	
+	public void refill(Context context, Cursor cursor)
+	{
+		changeCursor(cursor);
+	}
+}
