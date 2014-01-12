d49de584ba0df1a523f07737839ce5911989128b
Petr Nohejl <petr.nohejl@gmail.com>
2014-01-12 21:21:41 +0100

    Timer

---
+++ Src-Timer/src/fragment/ExampleFragment.java
@@ -0,0 +1,81 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.R;
+import com.example.utility.Logcat;
+
+
+public class ExampleFragment extends Fragment
+{
+	private final long TIMER_DELAY = 5000; // in milliseconds
+	
+	private View mRootView;
+	private Handler mTimerHandler;
+	private Runnable mTimerRunnable;
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		
+		// init timer task
+		mTimerHandler = new Handler();
+		mTimerRunnable = new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				Logcat.d("Fragment.timerRunnable()");
+				
+				mTimerHandler.postDelayed(this, TIMER_DELAY);
+			}
+		};
+	}
+	
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{	
+		mRootView = inflater.inflate(R.layout.fragment_example, container, false);
+		return mRootView;
+	}
+	
+	
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+		
+		// timer
+		startTimer();
+	}
+	
+	
+	@Override
+	public void onPause()
+	{
+		super.onPause();
+		
+		// timer
+		stopTimer();
+	}
+	
+	
+	private void startTimer()
+	{
+		mTimerHandler.postDelayed(mTimerRunnable, 0);
+	}
+	
+	
+	private void stopTimer()
+	{
+		mTimerHandler.removeCallbacks(mTimerRunnable);
+	}
+}
