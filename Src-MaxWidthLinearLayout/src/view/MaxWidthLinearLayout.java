cc9c4917772191fa1ce1327500141bc8d1950eac
Petr Nohejl <petr.nohejl@gmail.com>
2014-02-12 23:20:23 +0100

    MaxWidthLinearLayout

---
+++ Src-MaxWidthLinearLayout/src/view/MaxWidthLinearLayout.java
@@ -0,0 +1,42 @@
+package com.example.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import com.example.R;
+
+
+public class MaxWidthLinearLayout extends LinearLayout
+{
+	private int mMaxWidth;
+
+
+	public MaxWidthLinearLayout(Context context)
+	{
+		super(context);
+		mMaxWidth = 0;
+	}
+
+
+	public MaxWidthLinearLayout(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+		TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.MaxWidthLinearLayout);
+		mMaxWidth = typedArray.getDimensionPixelSize(R.styleable.MaxWidthLinearLayout_maxWidth, Integer.MAX_VALUE);
+	}
+
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+	{
+		int measuredWidth = MeasureSpec.getSize(widthMeasureSpec);
+		if(mMaxWidth>0 && mMaxWidth<measuredWidth)
+		{
+			int measureMode = MeasureSpec.getMode(widthMeasureSpec);
+			widthMeasureSpec = MeasureSpec.makeMeasureSpec(mMaxWidth, measureMode);
+		}
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
+}
