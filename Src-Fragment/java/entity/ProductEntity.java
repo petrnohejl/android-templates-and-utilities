26799f2700b9e34810ce271b8a8d2ab73be649e0
Petr Nohejl <petr.nohejl@gmail.com>
2015-01-05 00:23:26 +0100

    Fragment for Lollipop

--- Src-Fragment/java/entity/ProductEntity.java
+++ Src-Fragment/java/entity/ProductEntity.java
@@ -10,30 +10,36 @@ public class ProductEntity
 	// empty constructor
 	public ProductEntity()
 	{
-	
+
 	}
-	
-	
+
+
 	// copy constructor
 	public ProductEntity(ProductEntity origin)
 	{
 		id = origin.id;
 		if(origin.name!=null) name = new String(origin.name);
 	}
-	
-	
+
+
 	public long getId()
 	{
 		return id;
 	}
+
+
 	public void setId(long id)
 	{
 		this.id = id;
 	}
+
+
 	public String getName()
 	{
 		return name;
 	}
+
+
 	public void setName(String name)
 	{
 		this.name = name;
--- Src-Fragment/java/fragment/SimpleFragment.java
+++ Src-Fragment/java/fragment/SimpleFragment.java
@@ -2,7 +2,6 @@ package com.example.fragment;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -22,7 +21,6 @@ import com.example.view.ViewState;
 
 public class SimpleFragment extends TaskFragment implements OnLoadDataListener
 {
-	private boolean mActionBarProgress = false;
 	private ViewState mViewState = null;
 	private View mRootView;
 	private LoadDataTask mLoadDataTask;
@@ -78,9 +76,6 @@ public class SimpleFragment extends TaskFragment implements OnLoadDataListener
 		{
 			showEmpty();
 		}
-		
-		// progress in action bar
-		showActionBarProgress(mActionBarProgress);
 	}
 	
 	
@@ -206,14 +201,6 @@ public class SimpleFragment extends TaskFragment implements OnLoadDataListener
 		{
 			showOffline();
 		}
-	}
-	
-	
-	private void showActionBarProgress(boolean visible)
-	{
-		// show action bar progress
-		((ActionBarActivity) getActivity()).setSupportProgressBarIndeterminateVisibility(visible);
-		mActionBarProgress = visible;
 	}
 	
 	
