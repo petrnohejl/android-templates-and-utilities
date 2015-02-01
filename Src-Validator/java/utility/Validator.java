7c39724f9cacd4c54a09094c212cd8db639ac3ff
Petr Nohejl <petr.nohejl@gmail.com>
2015-02-01 18:56:12 +0100

    Date validation

--- Src-Validator/java/utility/Validator.java
+++ Src-Validator/java/utility/Validator.java
@@ -2,10 +2,13 @@ package com.example.utility;
 
 import android.util.Patterns;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 
 public class Validator
 {
-	public static boolean checkEmail(CharSequence email)
+	public static boolean isEmailValid(CharSequence email)
 	{
 		if(email==null)
 		{
@@ -15,5 +18,29 @@ public class Validator
 		{
 			return Patterns.EMAIL_ADDRESS.matcher(email).matches();
 		}
+	}
+
+
+	public static boolean isDateValid(String date, String format)
+	{
+		if(date==null)
+		{
+			return false;
+		}
+
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+		simpleDateFormat.setLenient(false);
+
+		try
+		{
+			simpleDateFormat.parse(date);
+		}
+		catch(ParseException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
 	}
 }
