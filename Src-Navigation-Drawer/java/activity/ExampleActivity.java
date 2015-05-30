3ada4986b43d52966cfeccc562ee648997bcdee9
Petr Nohejl <petr.nohejl@gmail.com>
2015-05-30 22:10:40 +0200

    Handle back button in navigation drawer

--- Src-Navigation-Drawer/java/activity/ExampleActivity.java
+++ Src-Navigation-Drawer/java/activity/ExampleActivity.java
@@ -7,9 +7,10 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -97,8 +98,22 @@ public class ExampleActivity extends AppCompatActivity
 		super.onConfigurationChanged(newConfiguration);
 		mDrawerToggle.onConfigurationChanged(newConfiguration);
 	}
-	
-	
+
+
+	@Override
+	public void onBackPressed()
+	{
+		if(mDrawerLayout.isDrawerOpen(Gravity.LEFT))
+		{
+			mDrawerLayout.closeDrawer(Gravity.LEFT);
+		}
+		else
+		{
+			super.onBackPressed();
+		}
+	}
+
+
 	@Override
 	public void setTitle(CharSequence title)
 	{
