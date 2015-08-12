33c26ad5a19313165cc1f529c6cc9820ed31e382
Petr Nohejl <petr.nohejl@gmail.com>
2015-08-12 18:22:02 +0200

    Resources utility

---
+++ Src-ResourcesUtility/java/utility/ResourcesUtility.java
@@ -0,0 +1,17 @@
+package com.example.utility;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.TypedValue;
+
+
+public class ResourcesUtility
+{
+	public static int getValueOfAttribute(Context context, int attr)
+	{
+		TypedValue typedValue = new TypedValue();
+		Resources.Theme theme = context.getTheme();
+		theme.resolveAttribute(attr, typedValue, true);
+		return typedValue.data;
+	}
+}
