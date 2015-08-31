8151e2878fbd7fb90975179503df4ca54bfb6c64
Petr Nohejl <petr.nohejl@gmail.com>
2015-08-31 18:50:01 +0200

    TabLayout

---
+++ Src-ViewPager/java/activity/TabLayoutActivity.java
@@ -0,0 +1,51 @@
+package com.example.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.R;
+import com.example.adapter.TabLayoutFragmentPagerAdapter;
+
+
+public class TabLayoutActivity extends AppCompatActivity
+{
+	private TabLayoutFragmentPagerAdapter mAdapter;
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tablayout);
+		bindData();
+	}
+	
+	
+	private void bindData()
+	{
+		// reference
+		ViewPager viewPager = (ViewPager) findViewById(R.id.activity_tablayout_pager);
+		TabLayout tabLayout = (TabLayout) findViewById(R.id.activity_tablayout_tabs);
+		
+		// pager content
+		if(mAdapter==null)
+		{
+			// create adapter
+			mAdapter = new TabLayoutFragmentPagerAdapter(getSupportFragmentManager());
+		}
+		else
+		{
+			// refill adapter
+			mAdapter.refill();
+		}
+
+		// set adapter
+		viewPager.setAdapter(mAdapter);
+
+		// tab layout
+		tabLayout.setupWithViewPager(viewPager);
+		tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+	}
+}
