a6eb4fc85fe15463f2b71c5cf9d7b666c5c2f564
Petr Nohejl <petr.nohejl@gmail.com>
2016-03-11 10:32:51 +0100

    ResourcesUtility

--- Src-ResourcesUtility/java/utility/ResourcesUtility.java
+++ Src-ResourcesUtility/java/utility/ResourcesUtility.java
@@ -2,6 +2,8 @@ package com.example.utility;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.util.TypedValue;
 
 
@@ -16,5 +18,41 @@ public final class ResourcesUtility
 		Resources.Theme theme = context.getTheme();
 		theme.resolveAttribute(attr, typedValue, true);
 		return typedValue.data;
+	}
+
+
+	public static int getColorValueOfAttribute(Context context, int attr)
+	{
+		TypedArray typedArray = context.obtainStyledAttributes(null, new int[]{attr}, 0, 0);
+		int value = typedArray.getColor(0, 0);
+		typedArray.recycle();
+		return value;
+	}
+
+
+	public static float getDimensionValueOfAttribute(Context context, int attr)
+	{
+		TypedArray typedArray = context.obtainStyledAttributes(null, new int[]{attr}, 0, 0);
+		float value = typedArray.getDimension(0, 0);
+		typedArray.recycle();
+		return value;
+	}
+
+
+	public static int getDimensionPixelSizeValueOfAttribute(Context context, int attr)
+	{
+		TypedArray typedArray = context.obtainStyledAttributes(null, new int[]{attr}, 0, 0);
+		int value = typedArray.getDimensionPixelSize(0, 0);
+		typedArray.recycle();
+		return value;
+	}
+
+
+	public static Drawable getDrawableValueOfAttribute(Context context, int attr)
+	{
+		TypedArray typedArray = context.obtainStyledAttributes(null, new int[]{attr}, 0, 0);
+		Drawable value = typedArray.getDrawable(0);
+		typedArray.recycle();
+		return value;
 	}
 }
