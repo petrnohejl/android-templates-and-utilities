6a5470371b6e46a8e1c1891282ce8315f63b3010
Petr Nohejl <petr.nohejl@gmail.com>
2017-06-12 16:58:06 +0200

    Remove old unnecessary code

--- Src-Application-Class/java/ExampleApplication.java
+++ Src-Application-Class/java/ExampleApplication.java
@@ -25,16 +25,6 @@ public class ExampleApplication extends Application
 	public void onCreate()
 	{
 		super.onCreate();
-
-		// force AsyncTask to be initialized in the main thread due to the bug:
-		// http://stackoverflow.com/questions/4280330/onpostexecute-not-being-called-in-asynctask-handler-runtime-exception
-		try
-		{
-			Class.forName("android.os.AsyncTask");
-		}
-		catch(ClassNotFoundException e)
-		{
-			e.printStackTrace();
-		}
+		// TODO
 	}
 }
