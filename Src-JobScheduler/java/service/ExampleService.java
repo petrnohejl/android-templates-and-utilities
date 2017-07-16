a69e6c6f6b5609d3f5d973998728c41b349cccc6
Petr Nohejl <petr.nohejl@gmail.com>
2017-07-16 23:04:06 +0200

    JobScheduler

---
+++ Src-JobScheduler/java/service/ExampleService.java
@@ -0,0 +1,49 @@
+package com.example.service;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import org.alfonz.utility.Logcat;
+
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class ExampleService extends JobService
+{
+	@Override
+	public boolean onStartJob(JobParameters params)
+	{
+		doJob(params);
+		return true;
+	}
+
+
+	@Override
+	public boolean onStopJob(JobParameters params)
+	{
+		return true;
+	}
+
+
+	private void doJob(final JobParameters params)
+	{
+		new Thread(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
+					Logcat.d("doing job");
+					Thread.sleep(3000L);
+					jobFinished(params, false);
+				}
+				catch(InterruptedException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}).start();
+	}
+}
---
+++ Src-JobScheduler/java/utility/JobSchedulerUtility.java
@@ -0,0 +1,32 @@
+package com.example.utility;
+
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import com.example.service.ExampleService;
+
+
+public final class JobSchedulerUtility
+{
+	public static final int EXAMPLE_JOB = 0;
+
+
+	private JobSchedulerUtility() {}
+
+
+	@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+	public static void scheduleExampleJob(Context context)
+	{
+		ComponentName componentName = new ComponentName(context, ExampleService.class);
+
+		JobInfo.Builder builder = new JobInfo.Builder(EXAMPLE_JOB, componentName);
+		builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED);
+
+		JobScheduler jobScheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+		jobScheduler.schedule(builder.build());
+	}
+}
