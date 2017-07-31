2bb5a52125162956c7cd42306052bf07cd37ff13
Petr Nohejl <petr.nohejl@gmail.com>
2017-07-31 18:30:31 +0200

    Splash screen

---
+++ Src-Splash-Screen/java/activity/ExampleActivity.java
@@ -0,0 +1,18 @@
+package com.example.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.R;
+
+
+public class ExampleActivity extends AppCompatActivity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		setTheme(R.style.Theme_Example_Light);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_example);
+	}
+}
